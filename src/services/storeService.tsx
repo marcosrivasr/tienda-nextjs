cf4207642447470301e285e2291e9bf9d8e3ebb5
Orangel Jose Gonzalez <goranjose16@gmail.com>
2023-01-25 16:30:40 -0400

    Add axios

--- src/services/storeService.tsx
+++ src/services/storeService.tsx
@@ -1,14 +1,6 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "http://localhost:3000",
-  timeout: 1000,
-  headers: { "X-Custom-Header": "foobar" },
-});
-
 export async function getItems() {
-  const request = await instance.get("http://localhost:3000/api/items");
-  const items = await request.data;
+  const request = await fetch("http://localhost:3000/api/items");
+  const items = await request.json();
 
   return items;
 }
