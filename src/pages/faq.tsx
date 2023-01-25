ae1aa9f58295cfa46ca3a67545ad0396bf180f47
Orangel Jose Gonzalez <goranjose16@gmail.com>
2023-01-25 14:05:53 -0400

    Uptade files

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,5 +1,5 @@
 import Layout from "@/components/layout";
-import style from "../styles/faq.module.css";
+import style from "../../styles/faq.module.css";
 
 export default function FAQ() {
   return (
