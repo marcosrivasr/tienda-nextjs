3664ca0de3bcdb700b11089d8e1d5bfd50bfad53
Orangel Jose Gonzalez <goranjose16@gmail.com>
2023-01-25 22:23:34 -0400

    Update menu.tsx

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,43 +1,54 @@
 import Link from "next/link";
+import Image from "next/image";
 import { useAppContext } from "@/components/stateWrapper";
 import style from "../../styles/menu.module.css";
 
 export default function Menu() {
   const cart = useAppContext();
 
-  function handleClickCart(e: any) {
+  function handleClickCart(e) {
     e.preventDefault();
     cart.openCart();
   }
   return (
     <div className="my-[40px] mx-auto flex h-[100px] w-[90%] justify-center ">
-      <nav className="fixed flex h-[100px] w-[90%] gap-[40px] z-[1]">
-        <div className="flex w-[100%] items-center justify-center gap-4 rounded-[20px] shadow-nav-shadow bg-white">
+      <nav className="fixed z-[1] flex h-[80px] w-[90%] gap-[20px]">
+        <div className="flex w-[100%] items-center justify-center gap-4 rounded-[20px] bg-white shadow-nav-shadow">
           <Link
-            className="rounded-[10px] p-[20px] hover:bg-orange-400"
+            className="rounded-[10px] p-[20px] hover:bg-[#E24648]"
             href="/"
           >
             Home
           </Link>
 
           <Link
-            className="rounded-[10px] p-[20px] hover:bg-orange-400"
+            className="rounded-[10px] p-[20px] hover:bg-[#E24648]"
             href="/store"
           >
             Store
           </Link>
 
           <Link
-            className="rounded-[10px] p-[20px] hover:bg-orange-400"
+            className="rounded-[10px] p-[20px] hover:bg-[#E24648]"
             href="/faq"
           >
             FAQ
           </Link>
         </div>
 
-        <div className="flex items-center w-[100px] bg-white rounded-[20px] shadow-nav-shadow justify-center">
+        <div className="flex w-[80px] items-center justify-center rounded-[20px] border-[2px] border-[#E24648] bg-white shadow-nav-shadow">
           <Link href="#" onClick={handleClickCart}>
-            Carrito ({cart.getNumberOfItems()})
+            <div className="flex h-[80px] w-[80px] items-center justify-center">
+              <div className="absolute right-[-5px] top-[-5px] flex h-[25px] w-[25px] items-center justify-center rounded-full bg-[#E24648]">
+                <span className="text-[14px] text-white">{cart.getNumberOfItems()}</span>
+              </div>
+              <Image
+                height={40}
+                width={40}
+                src="https://www.mbhouse.ca/assets/img/icons/mb_cart_red.svg"
+                alt="shopping cart"
+              />
+            </div>
           </Link>
         </div>
       </nav>
