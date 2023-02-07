49490d52bad4f7c469a61c773b5c281249baa8a4
Orangel Jose Gonzalez <goranjose16@gmail.com>
2023-02-07 10:39:45 -0400

    Add component carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,13 @@
 import Layout from "@/components/layout";
 import Product from "@/components/product";
 import { getLatestItems } from "@/services/storeService";
-
+import Carousel from "@/components/carousel"
+ 
 export default function Home({ items }: any) {
   return (
     <Layout title="Home">
-      <div className="mb-[30px] flex h-full w-full justify-center">
-        <div className="h-screen w-[95%] rounded-[2px] bg-black bg-banner-1 bg-cover">
-          <div className="w-[50%] p-[40px]">
-            <h2 className="mt-[40px] mb-[30px] text-[5rem] font-bold text-white">
-              Shop the Winter 2023 Collection
-            </h2>
-            <p className="text-[1.6rem] text-white">
-              Level up your comfort this season with our new Winter Collection —
-              all new joggers, beanies, drinkware, and for the first time ever,
-              Octocookie cutters!
-            </p>
-          </div>
-        </div>
+      <div className="mb-[30px] flex h-full w-full items-end justify-center">
+        <Carousel />
       </div>
 
       <h3 className="mb-[10px] text-[2rem] font-medium">Latest Products</h3>
